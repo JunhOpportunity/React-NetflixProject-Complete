23a9084d6612e39a29091388fcac8cf1992db266
JunHo Kim <89464762+JunhOpportunity@users.noreply.github.com>
2022-09-27 16:02:34 +0900

    Initial commit

---
+++ src/Components/Slider.tsx
@@ -0,0 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
+import styled from "styled-components";
+import { getMovies, IGetMoviesResult } from "../api";
+import { makeImagePath } from "../utils";
+import { motion, AnimatePresence, useScroll } from "framer-motion";
+import { useState } from "react";
+import { useMatch, useNavigate, PathMatch } from "react-router-dom";
